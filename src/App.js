afd5a907629323f8aa7d98fe934a0fab5cfed48e
MansiDhandha <mansidhandha23@gmail.com>
2026-01-28 20:34:11 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState , useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Welcome from './components/pages/Welcome';
 import Login from './components/pages/Login';
